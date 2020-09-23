9b8176a5aa1dee9d09e0e78c498459cdff1ec1c8
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2020-09-23 09:35:51 +0100

    cache service előkészítése

--- src/app/services/cache.service.ts
+++ src/app/services/cache.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CacheService {
 
-  constructor() { }
+  private calcHistorySubject: BehaviorSubject<string[]>;
+  private calcHistory: Observable<string[]>;
+
+  constructor() {
+    this.calcHistorySubject = new BehaviorSubject<string[]>(JSON.parse(localStorage.getItem('calcHistory')).slice(0, 9) || []);
+    this.calcHistory = this.calcHistorySubject.asObservable();
+  }
 }
