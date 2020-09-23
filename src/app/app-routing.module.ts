e6b6b922574eb6d7a2808b79e0a57acdf952a76f
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2020-09-23 09:49:52 +0100

    routing modul beállítása

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {CalculatorComponent} from './calculator/calculator.component';
+import {environment} from '../environments/environment';
 
-
+const routes: Routes = [
+  { path: ':lang/calculator', component: CalculatorComponent },
+  { path: '**', redirectTo: `${environment.defaultLanguage}/calculator`, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ]
 })
 export class AppRoutingModule { }
