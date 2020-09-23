9c3db0c7256b1bbd6f48e8b5794ab5d98829b89f
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2020-09-23 09:34:04 +0100

    event service sidebarclick event emitter hozzáadása

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+
+  public sidebarclick = new EventEmitter<{ expression: string, value: number }>();
 
   constructor() { }
 }
